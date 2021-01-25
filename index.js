e3e78ba470dcb56e7af5d4cdc6f8d16f491ce03c
ocio28 <ocio28@gmail.com>
2021-01-25 13:44:57 -0300

    redux persist

--- index.js
+++ index.js
@@ -10,15 +10,26 @@ import {name as appName} from './app.json';
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunkMiddleware from 'redux-thunk'
+import FilesystemStorage from 'redux-persist-filesystem-storage'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore, persistReducer } from 'redux-persist'
 
 import reducers from "./src/reducers";
 
+const persistConfig = {
+  key: 'collector-app',
+  storage: FilesystemStorage
+}
 
-const store = createStore(reducers, applyMiddleware(thunkMiddleware))
+const persistedReducer = persistReducer(persistConfig, reducers)
+const store = createStore(persistedReducer, applyMiddleware(thunkMiddleware))
+const persistor = persistStore(store)
 
 const Root = () => (
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 )
 
